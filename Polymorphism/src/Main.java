36e4916be6656a8e8d4c292eca5b4a29da9aa31b
İlker Yelekçioğlu <53173162+IlkerYelekcioglu@users.noreply.github.com>
2023-03-29 21:13:24 +0300

    Add files via upload

---
+++ Polymorphism/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+   public static void main(String[] args) {
+////     EmailLogger logger = new EmailLogger();
+////     logger.Log("Log Mesajı ");
+////
+////
+////        BaseLogger[] loggers = new BaseLogger[]{new FileLogger(),new EmailLogger(),new DatabaseLogger(),new ConsoleLogger() };
+////        for(BaseLogger logger:loggers){
+////            logger.Log("Log Mesajı");
+//
+//
+//        }
+//    }
+      CustomerManager customerManager = new CustomerManager(new FileLogger());
+      customerManager.add();
+}
+}
