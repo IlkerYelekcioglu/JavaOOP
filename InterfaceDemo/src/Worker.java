6fb42ce1000e200c06646212b8fe322250144ed5
İlker Yelekçioğlu <53173162+IlkerYelekcioglu@users.noreply.github.com>
2023-03-30 17:35:55 +0300

    Add files via upload

---
+++ InterfaceDemo/src/Worker.java
@@ -0,0 +1,18 @@
+//Bizim çalışanımız
+public class Worker implements IWorkable,IEatable,IPayable {
+
+    @Override
+    public void Work() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void Pay() {
+
+    }
+}
