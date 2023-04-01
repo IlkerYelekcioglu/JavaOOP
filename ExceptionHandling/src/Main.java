b36a6e7de88ffca09c1ee4b4f33e7a40dd9ad932
İlker Yelekçioğlu <53173162+IlkerYelekcioglu@users.noreply.github.com>
2023-04-01 15:28:19 +0300

    Add files via upload

---
+++ ExceptionHandling/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        try{
+            int[] numbers = new int[] {1,2,3};
+            System.out.println(numbers[4]);
+        }catch (StringIndexOutOfBoundsException e){
+             System.out.println(e);
+        }catch (ArrayIndexOutOfBoundsException exception){
+            System.out.println(exception);
+        }catch (Exception exception){
+            System.out.println("Loglandı : " + exception);
+        }finally {
+            System.out.println("Ben her türlü çalışırım");
+        }
+    }
+}
